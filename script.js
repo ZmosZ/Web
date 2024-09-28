cf8a2476a4a4802b3d9f95588a14721a3c50d7cf
ZmosZ <154952681+ZmosZ@users.noreply.github.com>
2024-09-28 20:59:48 +0700

    mid day1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const menu = document.querySelector("#menu")
+const nav = document.querySelector(".links")
+
+menu.onclick = () =>{
+    menu.classList.toggle('bo-x');
+    nav.classList.toggle('active');
+}
+
 function changeImage(imageId, newSrc) {
     timeoutId = setTimeout(function() {
         document.getElementById(imageId).src = newSrc;

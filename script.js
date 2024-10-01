89101761245410ede12879bcc5681b46bfb9f1c8
ZmosZ <154952681+ZmosZ@users.noreply.github.com>
2024-10-01 22:58:32 +0700

    fix info

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const menu = document.querySelector("#menu")
-const nav = document.querySelector(".links")
+// การทำงานของเมนู เมื่อคลิกแล้วจะเปิดหรือปิดเมนู
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+let timeoutId;  // ประกาศตัวแปร timeoutId เป็น global
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('bo-x');
     nav.classList.toggle('active');
 }
 
+// ปิดเมนูอัตโนมัติเมื่อคลิกที่ลิงก์ใดๆ ในเมนู
+document.querySelectorAll('.links a').forEach(link => {
+    link.onclick = () => {
+        menu.classList.remove('bo-x');
+        nav.classList.remove('active');
+    }
+});
+
+// ฟังก์ชันสำหรับการเปลี่ยนภาพเมื่อวางเมาส์บนภาพ (เปลี่ยนภาพหลังจากรอ 100ms)
 function changeImage(imageId, newSrc) {
     timeoutId = setTimeout(function() {
         document.getElementById(imageId).src = newSrc;
-      }, 100);
-  }
-  function resetImage(imageId, originalSrc) {
-    clearTimeout(timeoutId);
-    document.getElementById(imageId).src = originalSrc;
-  }
+    }, 100);
+}
+
